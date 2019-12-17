206b6451f88b3103a5266d8af457cc2970c97605
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2019-12-17 19:42:04 -0300

    Att Register

--- register.php
+++ register.php
@@ -8,52 +8,54 @@
   	<link href="https://fonts.googleapis.com/css?family=Dosis|Staatliches&display=swap" rel="stylesheet">
   </head>
   <body class="bodyLogin">
-        <div class="elementLeft">
-            <img class="elementHand" src="img/Src/hand.png" alt="hand/mão">
-            <img class="elementPs4" src="img/Src/controlePS4.png" alt="controle ps4">
-            <img class="elementXBOX" src="img/Src/controleXBOX.png" alt="controle ps4">
-        </div>
-            <h1>Cadastre-se:</h1>
-            <form action="emailConfirmation.php" method="POST">
-              <div class="boxCadastro">
-                <center>
-                  <div class="inputBox">
-                    <input type="text" name="txtEmail" maxlength="50" required>
-                    <label>E-Mail</label>
-                  </div>
-                  <div class="inputBox">
-                    <input type="text" name="txtCEmail" maxlength="50" required>
-                    <label>Confirmar E-Mail</label>
-                  </div>
-                  <div class="inputBox">
-                    <input type="password" name="txtPwd" maxlength="16" required>
-                    <label>Senha</label>
-                  </div>
-                  <div class="inputBox">
-                    <input type="password" name="txtCPwd" maxlength="16" required>
-                    <label>Confirmar senha</label>
-                  </div>
-                  <button class="buttonCadastro" name="btnRegister">Cadastrar</button>
-                  <br>
-                </center>
-              </div>
-            </form>
-    
+    <div class="elementLeft">
+      <img class="elementHand" src="img/Src/hand.png" alt="hand/mão">
+      <img class="elementPs4" src="img/Src/controlePS4.png" alt="controle ps4">
+      <img class="elementXBOX" src="img/Src/controleXBOX.png" alt="controle ps4">
+    </div>
+    <h1>Cadastre-se:</h1>
+    <form action="" method="POST">
+      <div class="boxCadastro">
+        <?php
+          session_start();
+          if ($_SESSION['error'] != ''):
+            echo $_SESSION['error'];
+          endif;
+        ?>
+        <center>
+          <div class="inputBox">
+            <input type="text" name="txtEmail" maxlength="50" autocomplete="off" autofocus required>
+            <label>E-Mail</label>
+          </div>
+          <div class="inputBox">
+            <input type="text" name="txtCEmail" maxlength="50" autocomplete="off" required>
+            <label>Confirmar E-Mail</label>
+          </div>
+          <div class="inputBox">
+            <input type="password" name="txtPwd" maxlength="16" autocomplete="off" required>
+            <label>Senha</label>
+          </div>
+          <div class="inputBox">
+            <input type="password" name="txtCPwd" maxlength="16" autocomplete="off" required>
+            <label>Confirmar senha</label>
+          </div>
+          <button class="buttonCadastro" name="btnRegister">Cadastrar</button>
+          <br>
+        </center>
+      </div>
+    </form>
+
     <?php
       if (isset($_POST['btnRegister'])):
         // Parâmetros que serão enviados
-        $e = $_POST['txtEmail'];
-        $pwd = $_POST['txtPwd'];
-
+        $_SESSION['emailRegister']  = $_POST['txtEmail'];
+        $_SESSION['pwdRegister']    = $_POST['txtPwd'];
         // Verificando se o E-Mail e a senha são correspondentes
         if ($_POST['txtEmail'] == $_POST['txtCEmail'] && $_POST['txtPwd'] == $_POST['txtCPwd']):
-          include 'includes/Register.php';
-          $conn = new Register();
-           // Passando os Parâmetros
-          $conn->createUser($fn, $ln, $u, $e, $pwd, $p);
+          header('location: sendGrid/emailConfirmation.php?true=true');
         else:
           // Caso a estrutura de decisão não for verdadeira, esta linha retorna um erro
-          echo "<h6 style='color: white;'> Senha ou E-Mail não correspondentes </h6>";
+          $_SESSION['error'] = "<h6 style='color: red;'> Senha ou E-Mail não correspondentes </h6><br>";
         endif;
       endif;
     ?>
---
+++ sendGrid/emailConfirmation.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WinnChay - Login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Dosis|Staatliches&display=swap" rel="stylesheet">
+</head>
+<body class="bodyLogin">
+	<header>
+		Foi enviado um E-mail com o Código de verificação
+	</header>
+	<div class="container">
+		<center>
+		<?php if (isset($_GET['true'])): ?>
+			<div class="element_confirmEmail">
+				<form class="" action="" method="post">
+					<p>Digite o Codigo que foi enviado em seu E-mail:</p>
+					<input type="text" name="txtCodigo">
+					<br><br>
+					<button name="btnRecover" class="buttonCadastro">Confirmar</button>
+					<button name="btnRepeat" class="buttonCadastro">Reenviar Código</button>
+				</form>
+			</div>
+		<?php elseif (isset($_POST['btnRepeat'])):?>
+			<div class="element_confirmEmail">
+				<form action="" method="post">
+					<p>Código reenviado</p>
+					<p>Digite o Codigo que foi enviado em seu E-mail:</p>
+					<input type="text" name="txtCodigo">
+					<br><br>
+					<button name="btnRecover" class="buttonCadastro">Confirmar</button>
+					<button name="btnRepeat" class="buttonCadastro">Reenviar Código</button>
+				</form>
+			</div>
+		<?php endif; ?>
+		<?php
+			session_start();
+			$_SESSION['cod'] = rand(1000, 9999);
+			// Requisitando o composer
+			require 'vendor/autoload.php';
+
+			// Definindo os parâmetros de envio da mensagem
+			$from = new SendGrid\Email(null, "WinnChay@suporte.com");
+			$subject = "Ativar Conta - WinnChay";
+			$to = new SendGrid\Email(null, "{$_SESSION["emailRegister"]}");
+			$content = new SendGrid\Content("text/html", "Olá, <br><br>ative seu código de ativação de conta: <br><br><b> Código: </b>{$_SESSION['cod']}.<br><br> Atenciosamente,<br><b> Equipe WinnChay. </b>");
+			$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+			// Ativando a chave da API no SednGrid
+			$apiKey = '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';
+			$sg = new \SendGrid($apiKey);
+
+			//Finalizando o processo
+			$response = $sg->client->mail()->send()->post($mail);
+
+			if (isset($_POST['btnRepeat'])):
+				$_SESSION['cod'] = rand(1000, 9999);
+
+				// Requisitando o composer
+				require 'vendor/autoload.php';
+
+				// Definindo os parâmetros de envio da mensagem
+				$from = new SendGrid\Email(null, "WinnChay@suporte.com");
+				$subject = "Ativar Conta - WinnChay";
+				$to = new SendGrid\Email(null, "{$_SESSION["emailRegister"]}");
+				$content = new SendGrid\Content("text/html", "Olá, <br><br>ative seu código de ativação de conta: <br><br><b> Código: </b>{$_SESSION['cod']}.<br><br> Atenciosamente,<br><b> Equipe WinnChay. </b>");
+				$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+				// Ativando a chave da API no SednGrid
+				$apiKey = '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';
+				$sg = new \SendGrid($apiKey);
+
+				//Finalizando o processo
+				$response = $sg->client->mail()->send()->post($mail);
+				echo "<br><h6 style='text-align:center;color: #925EFF;'>Código enviado com sucesso!</h6>";
+			elseif (isset($_POST['btnRecover'])):
+				// Verificando se o código digitado é o mesmo que o enviado para o email
+				if ($_SESSION['cod'] == $_POST['txtCodigo']):
+					header('location:../includes/register.php');
+				else:
+					echo "<br><h6 style='text-align:center;color: #925EFF;'>Código digitado incorreto!!</h6>";
+				endif;
+			endif;
+		?>
+		</center>
+	</div>
+</body>
+</html>
